822eb58394379da407364c3060e69eaebf7dd256
Marvin Kome <marvinkome@gmail.com>
2022-11-03 23:00:58 +0100

    add roadmap section

---
+++ icons/arrow.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { chakra } from "@chakra-ui/react";
+
+const ArrowIcon = (props) => (
+  <chakra.svg viewBox="0 0 12 48" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M6 47.7735L11.7735 42L6 36.2265L0.226497 42L6 47.7735ZM5 0L5 42H7L7 0L5 0Z" fill="white" />
+  </chakra.svg>
+);
+
+export default ArrowIcon;
