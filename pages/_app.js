057fcf8e90a3995cbbcaf8fb31110e82398436b0
Sophia Abah <sophiaabah@gmail.com>
2022-11-14 09:48:09 +0100

    ga added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "theme";
+import { useRouter } from "next/router";
+import ReactGA from "react-ga4";
 
 function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    ReactGA.initialize(process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID);
+    ReactGA.send({ hitType: "pageview", page: router.asPath });
+
+    const handleRouteChange = (url) => {
+      ReactGA.send({ hitType: "pageview", page: url });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router]);
+
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
